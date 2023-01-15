420940b746889fd9626f596520a8ced7a0b8f350
Scarlet20030225 <21010022hs@edu.fca.ac.jp>
2023-01-15 16:53:34 +0900

    GameObject.hのコードを記入

--- GameObjectManager.h
+++ GameObjectManager.h
@@ -1,1 +1,29 @@
 #pragma once
+#include<DxLib.h>
+#include<vector>
+#include<unordered_map>
+#include"GameObject.h"
+#include"ObjectTag.h"
+#include"Math.h"
+
+namespace App
+{
+	class GameObjectManager final
+	{
+	public:
+		static void Init();
+		static void Entry(GameObject* newObj);
+		static void Release(GameObject* releaseObj);
+		static void ReleaseAllObj();
+		static void Update(float deltaTime);
+		static void Draw();
+		static void Finalize();
+
+	private:
+		GameObjectManager();
+		~GameObjectManager();
+		static GameObjectManager* mpInstance;
+		std::vector<GameObject*>  mPendingObjects;
+		std::vector<GameObject*>  mObjects;
+	};
+}
