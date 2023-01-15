358ebdd7a546cd78f235c875720b5395a2941930
Scarlet20030225 <21010022hs@edu.fca.ac.jp>
2023-01-15 15:39:53 +0900

    AssetManager.hのコードを記入

--- AssetManager.h
+++ AssetManager.h
@@ -1,1 +1,29 @@
 #pragma once
+#include<string>
+#include<unordered_map>
+#include<atlstr.h>
+#include<DxLib.h>
+
+namespace App
+{
+	class AssetManager final
+	{
+	public:
+		static void Init();
+		static int GetMesh(std::string meshFileName);
+		static int GetAnimation(std::string animationFileName);
+		static void ReleaseMesh(int meshID);
+		static void ReleaseAllAsset();
+		static void Finalize();
+
+	private:
+		AssetManager();
+		~AssetManager();
+
+		static AssetManager* mpInstance;
+
+		std::unordered_map<std::string, int>mMeshMap;
+		std::unordered_map<std::string, int>mAnimationMap;
+		std::vector<int>mDuplicatesMesh;
+	};
+}
