178419f2a64a3936e5a58ee99850f399bd7aef0b
Scarlet20030225 <21010022hs@edu.fca.ac.jp>
2023-01-16 12:22:03 +0900

    フレーム追加＆オブジェクトを表示するための関数をMain.cppに追加

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,10 @@
-#include "DxLib.h"
-
+#include"DxLib.h"
+#include"AssetManager.h"
+#include"GameObject.h"
+#include"GameObjectManager.h"
+#include"Collision.h"
+#include"Player.h"
+#include"PlayerShot.h"
 #pragma warning(disable:4996)
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -16,14 +21,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     SetCameraNearFar(1.0f, 2000.0f);
     SetCameraPositionAndTarget_UpVecY(VGet(0, 580, -200), VGet(0.0f, 80.0f, 0.0f));
 
+    App::GameObjectManager::Init();
+    App::AssetManager::Init();
 
+    App::Player* player = new App::Player;
+    App::GameObjectManager::Entry(player);
+
+    int nowTime = GetNowCount();
+    int prevTime = nowTime;
     while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
     {
+        nowTime = GetNowCount();
+        float deltaTime = (nowTime - prevTime) / 1000.0f;
+
+        char buf[256];
+        sprintf(buf, "deltaTime[%5.4f]", deltaTime);
+
+        App::GameObjectManager::Update(deltaTime);
+
         //âÊñ çXêVèàóù
         ClearDrawScreen();
-        ScreenFlip();
-    }
+        DrawString(100, 100, buf, GetColor(255, 255, 255));
 
+        App::GameObjectManager::Draw();
+
+        ScreenFlip();
+
+        prevTime = nowTime;
+    }
+    App::GameObjectManager::Finalize();
+    App::AssetManager::Finalize();
 
     DxLib_End();
     return 0;
