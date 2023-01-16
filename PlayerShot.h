35f7ea67014bb587684dc814658660af2dcfb870
Scarlet20030225 <21010022hs@edu.fca.ac.jp>
2023-01-16 11:43:19 +0900

    PlayerShot.hのコードを記入

--- PlayerShot.h
+++ PlayerShot.h
@@ -1,1 +1,25 @@
 #pragma once
+#include"DxLib.h"
+#include"math.h"
+#include"Collision.h"
+#include"GameObject.h"
+
+namespace App
+{
+	class PlayerShot : public GameObject
+	{
+	public:
+		PlayerShot(class Player* player);
+		~PlayerShot();
+
+		void Update(float deltaTime);
+		void Draw();
+
+		Lib::Sphere GetCollsionSphere() { return mCollisionSphere; }
+
+	private:
+		VECTOR mDir;
+		float  mSpeed;
+		Lib::Sphere mCollisionSphere;
+	};
+};
